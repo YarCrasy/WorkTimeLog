a2c310af93f052665996bc28c6b0e9ca1dca07b2
YarCrasy <108998893+YarCrasy@users.noreply.github.com>
2024-12-16 23:31:42 +0000

    adddisplay

--- WorkerPage.xaml.cs
+++ WorkerPage.xaml.cs
@@ -13,16 +13,44 @@ public partial class WorkerPage : ContentPage
 
         datePicker.Date = DateTime.Now.Date;
         timePicker.Time = DateTime.Now.TimeOfDay;
+
+        LoadWorkLogs();
     }
 
-    private void LogTimeClicked(object sender, EventArgs e)
+    private async void LogTimeClicked(object sender, EventArgs e)
     {
         WorkLog workLog = new()
         {
             Id = Guid.NewGuid().ToString(),
             UserNif = user.Nif,
             Date = datePicker.Date + timePicker.Time,
-            IsEntry = true
+            IsEntry = isEntry.IsToggled
         };
+
+        using (AppDbContext db = new())
+        {
+            db.WorkLogs.Add(workLog);
+            await db.SaveChangesAsync();
+        }
+
+        LoadWorkLogs();
+    }
+
+    private void LoadWorkLogs()
+    {
+        workLogStackLayout.Children.Clear();
+
+        using (AppDbContext db = new())
+        {
+            var workLogs = db.WorkLogs.Where(w => w.UserNif == user.Nif).ToList();
+
+            foreach (var log in workLogs)
+            {
+                workLogStackLayout.Children.Add(new Label
+                {
+                    Text = $"Fecha: {log.Date}, Entrada: {log.IsEntry}"
+                });
+            }
+        }
     }
 }
